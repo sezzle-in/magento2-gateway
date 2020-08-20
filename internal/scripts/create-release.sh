354c2ae6dd789254d28cbc9734d45f65a0e95ce6
Sankaran Kaliappan <mail@sankarank.in>
2020-08-20 09:50:55 +0530

    build script update

--- internal/scripts/create-release.sh
+++ internal/scripts/create-release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rm -rf sezzle-magento2 && mkdir -p sezzle-magento2/Sezzle/Sezzlepay
-cp README.md sezzle-magento2
-cp -R app/* sezzle-magento2/Sezzle/Sezzlepay/
-zip -r sezzle-magento2.zip sezzle-magento2
+rm -rf sezzle-magento2 && mkdir -p sezzle-magento2/Sezzle/Sezzlepay && \
+cp README.md sezzle-magento2 && \
+cp -R app/* sezzle-magento2/Sezzle/Sezzlepay/ && \
+zip -r sezzle-magento2.zip sezzle-magento2 && \
+rm -rf sezzle-magento2
